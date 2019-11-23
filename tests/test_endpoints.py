788fdf2ee08231b846d6e715fe54f449893fc64a
Janek Szynal <jan.szynal@gmail.com>
2019-11-23 20:06:53 +0100

    Add test_endpoints

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,6 +1,8 @@
 import subprocess
 
+
 def test_areyourunning():
-    result = subprocess.run(['curl','localhost:3141/areyourunning'], capture_output=True)
-    answer = result.stdout.decode("utf-8") 
-    assert answer  == 'YES'
+    result = subprocess.run(
+        ['curl', 'localhost:3141/areyourunning'], capture_output=True)
+    answer = result.stdout.decode("utf-8")
+    assert answer == 'YES'
