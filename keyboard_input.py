09a2b056002ede1463f90daab1eda1a716329306
Janek Szynal <jan.szynal@gmail.com>
2020-07-23 19:06:32 +0200

    Catch "," as an alternative to "."

--- keyboard_input.py
+++ keyboard_input.py
@@ -27,7 +27,7 @@ def on_press(key):
 			print("voldown")
 		elif key.char == "0":
 			print("0 caught")
-		elif key.char == "." or key.vk == 47:
+		elif key.char in [".", ","]:
 			playpause()
 		elif key.char in ['1','2','3','4','5','6','7','8','9']:
 			if numlock_modifier_on:
