6629dc0e44082c87f46619b93f4c3ffbbce42713
Janek Szynal <jan.szynal@gmail.com>
2020-07-26 16:22:09 +0200

    Update playlists to use timeline

--- playlists.py
+++ playlists.py
@@ -1,11 +1,11 @@
 playlists = {
-    '1': "spotify:user:1169770272:playlist:34MUylX38cHJ7vsVU7NXhP", # know you
-    '2': "spotify:user:1169770272:playlist:7iDYDZkSEzayWp43ZLauIZ", # kamixlo
-    '3': "spotify:user:1169770272:playlist:6ETQNQIn4ptCLag0qAcart", # izabela 
-    '4': "spotify:user:1169770272:playlist:34MUylX38cHJ7vsVU7NXhP", # know you
-    '5': "spotify:user:1169770272:playlist:7iDYDZkSEzayWp43ZLauIZ", # kamixlo
-    '6': "spotify:user:1169770272:playlist:6ETQNQIn4ptCLag0qAcart", # izabela 
-    '7': "spotify:user:1169770272:playlist:34MUylX38cHJ7vsVU7NXhP", # know you
-    '8': "spotify:user:1169770272:playlist:7iDYDZkSEzayWp43ZLauIZ", # kamixlo
-    '9': "spotify:user:1169770272:playlist:6ETQNQIn4ptCLag0qAcart", # izabela 
+    '1': "spotify:playlist:1LbN4ustcjHX1hV33PDBi5", # july
+    '2': "spotify:playlist:5QjNMwxzyVqypxqAPXxHuQ", # june
+    '3': "spotify:playlist:7a0C1ceRjHYcQ2jUG2wNCz", # ... 
+    '4': "spotify:playlist:0mWfdLQMV0b01ccha6em15", 
+    '5': "spotify:playlist:4EnkjbGV2dbp9nimXKkbmj", 
+    '6': "spotify:playlist:3b2rfiDGthhRgdpdJFPO1d", 
+    '7': "spotify:playlist:0znKrQijn03BIyfoSqoAWu", 
+    '8': "spotify:playlist:76mHokDY74xrfqkQypbazh", 
+    '9': "spotify:playlist:4hGSBQ2TIEqJlPQbJrphtq", 
 }
