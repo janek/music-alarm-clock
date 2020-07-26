d7362b25dea25b567e39d2849fb0768c49afeccc
Janek Szynal (nek-pi) <jan.szynal@gmail.com>
2020-07-26 16:57:04 +0200

    Update keyboard_input.py

--- keyboard_input.py
+++ keyboard_input.py
@@ -1,48 +1,58 @@
 from pynput.keyboard import Key, KeyCode, Listener
 import requests
 from playlists import playlists
-from server import play, pause, playpause, set_volume
+from server import play, pause, playpause, set_volume, previous, next, toggle_shuffle
 
 
-numlock_modifier_on = False
-
+numpad_keys_without_numlock = [Key.end, Key.down, Key.page_down, 
+							Key.left, Key.f1, Key.right, 
+							Key.home, Key.up, Key.page_up]
+	# Special case: we use f1 as a placeholder for '5',
+	# because 5 is not registered in numlock-off layer.
+	# (we handle it in the try block with numlock-on keys)
 
 def on_press(key):
-	global numlock_modifier_on
-	# 0-9 and /*.-+ are a `KeyCode` with `char` equal to the character on the key and some `vk`
-	# enter and numlock are a `KeyCode` with `char` empty but `vk` equal to 76 and 71
-	# backspace is a `Key` with enum value Key.backspace
+	#  __________
+	# | N / * - |
+	# | 7 8 9 + |
+	# | 4 5 6 B |
+	# | 1 2 3 E |
+	# | 0 0 D E |
+	#  __________
+	
+	# PAGE 2 (numlock off):
+	# Key.home, .up, .page_up
+	# .left, 65437, .right
+	# .end, .down, page_down
+	# .insert, .delete, (enter)
 	
 	try:
-		#TODO: how can this form be made clearer? a dictionary with code and action? remember vk vs char
-		print(key.vk, key.char)
-		if key.vk == 76:
-			print("got enter")
-			playpause()
-		elif key.vk == 71:
-			numlock_modifier_on = True
-			print("numlock on")
-		elif key.char == "-":	
-			print("volup")
-		elif key.char == "+":
-			print("voldown")
-		elif key.char == "0":
-			print("0 caught")
+		# Still unused: /, *, -, 0
+		if key.char == '+':
+			toggle_shuffle()
 		elif key.char in [".", ","]:
-			playpause()
+			previous()
 		elif key.char in ['1','2','3','4','5','6','7','8','9']:
-			if numlock_modifier_on:
-				set_volume(int(key.char))
-				print('set volume to ' + key.char)
-				numlock_modifier_on = False
-			else:
-				print("play num " + key.char)
-				play(spotify_uri=playlists[key.char])
-		
+			print("play num " + key.char)
+			play(spotify_uri=playlists[key.char])
+		elif key.vk == 65437:
+			# Special case: this is the "vk" value 
+			# of the number 5 when numlock is off.
+			# This key is not identifiable otherwise.
+			set_volume_step(5)
 	except AttributeError:
-		# Happens when key.vk or key.char is empty, i.e. for all `Key` objects
-		if key == Key.backspace:
-			print("Got backspace")
-
+		# Still unused (.insert)
+		if key == Key.enter: 
+			playpause()
+		elif key == Key.delete:
+			next()
+		elif key == Key.backspace:
+			previous()
+		elif key in numpad_keys_without_numlock:
+			set_volume_step(numpad_keys_without_numlock.index(key) + 1)
+		
+def set_volume_step(step):
+	set_volume(step*10)
+	
 with Listener(on_press=on_press) as listener:
 	listener.join()
