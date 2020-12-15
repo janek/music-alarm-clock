11513740c801ef24e806c5bc41e55a7a79a72f79
janek <jan.szynal@gmail.com>
2020-12-15 19:07:14 +0100

    basic play/pause for radio

--- keyboard_input.py
+++ keyboard_input.py
@@ -1,10 +1,21 @@
 from pynput.keyboard import Key, KeyCode, Listener
 import requests
-from server import radiostop, radioplay, radionext
-
+from server import say, radiostop, radioplay, radionext
 
 def on_press(key):
     print(key)
+    try:
+        if key.char == '/':
+            radiostop()
+            say("Stopping radio")
+        if key.char == '*':
+            radioplay()
+            say("Starting radio")
+        if key.char == '-':
+            radionext()
+            say("Playing next station")
+    except Exception:
+        print("Hit an exception")
 
 with Listener(on_press=on_press) as listener:
     listener.join()
