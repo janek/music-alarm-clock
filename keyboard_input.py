db8a9f5fc3761d0037b4c45119f4cb652c64c5f4
Janek <jan.szynal@gmail.com>
2024-01-14 19:45:28 +0100

    reindent all

--- keyboard_input.py
+++ keyboard_input.py
@@ -13,9 +13,7 @@ def on_press(key):
 	# 0-9 and /*.-+ are a `KeyCode` with `char` equal to the character on the key
 	# enter and numlock are a `KeyCode` with `char` empty but `vk` equal to 76 and 71
 	# backspace is a `Key` with enum value Key.backspace
-
 	try:
-		#TODO: how can this form be made clearer? a dictionary with code and action? remember vk vs char
 		print(key)
 		if key.vk == 76:
 			print("got enter")
@@ -23,7 +21,7 @@ def on_press(key):
 		elif key.vk == 71:
 			numlock_modifier_on = True
 			print("numlock on")
-		elif key.char == "-":	
+		elif key.char == "-":
 			print("volup")
 		elif key.char == "+":
 			print("voldown")
@@ -41,13 +39,30 @@ def on_press(key):
 			else:
 				print("play num " + key.char)
 				play(spotify_uri=playlists[key.char])
-		
-            
-
 	except AttributeError:
 		# Happens when key.vk or key.char is empty, i.e. for all `Key` objects
+		corresponding_number = get_number_for_numlocked_key(key)
+		if corresponding_number:
+			set_volume(corresponding_number)
 		if key == Key.backspace:
 			print("Got backspace")
+		if key == Key.enter:
+			print("Got enter")
+
+def get_number_for_numlocked_key(key):
+	key_map = {
+            Key.insert: 0,
+            Key.end: 1,
+            Key.down: 2,
+            Key.page_down: 3,
+            Key.left: 4,
+            "<65437>": 5,  # Assuming this is a specific key identification
+            Key.right: 6,
+            Key.home: 7,
+            Key.up: 8,
+            Key.page_up: 9
+        }
+	return key_map.get(key, None)
 
 with Listener(on_press=on_press) as listener:
 	listener.join()
