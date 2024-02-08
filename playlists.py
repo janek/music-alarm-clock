a9c2a3a43ef92ae70a7631505cefdfe59399d149
Janek <jan.szynal@gmail.com>
2024-02-08 15:49:07 +0100

    Modify playlists.py

--- playlists.py
+++ playlists.py
@@ -2,9 +2,9 @@ playlists = {
     '1': "spotify:user:1169770272:playlist:34MUylX38cHJ7vsVU7NXhP", # know you
     '2': "spotify:user:1169770272:playlist:7iDYDZkSEzayWp43ZLauIZ", # kamixlo
     '3': "spotify:user:1169770272:playlist:6ETQNQIn4ptCLag0qAcart", # izabela 
-    '4': "spotify:user:1169770272:playlist:34MUylX38cHJ7vsVU7NXhP", # know you
-    '5': "spotify:user:1169770272:playlist:7iDYDZkSEzayWp43ZLauIZ", # kamixlo
-    '6': "spotify:user:1169770272:playlist:6ETQNQIn4ptCLag0qAcart", # izabela 
+    '4': "spotify:user:1170444051:playlist:1p3Pb9O360fuQlr2m1upRd", # yaaaaz
+    '5': "spotify:user:1169770272:playlist:5crU6AclXGahkiVpvIcbZQ", # morning
+    '6': "spotify:user:1169770272:playlist:5crU6AclXGahkiVpvIcbZQ", # morning
     '7': "spotify:user:1169770272:playlist:34MUylX38cHJ7vsVU7NXhP", # know you
     '8': "spotify:user:1169770272:playlist:7iDYDZkSEzayWp43ZLauIZ", # kamixlo
     '9': "spotify:user:1169770272:playlist:6ETQNQIn4ptCLag0qAcart", # izabela 
